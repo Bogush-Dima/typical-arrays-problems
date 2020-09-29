c71881ff5e5bf081a70db0db2f974bd09f51a44d
Bogush-Dima <bogusd66@gmail.com>
2020-09-29 11:01:00 +0300

    feat: my decision this task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) {
+      return a - b
+    })
+    return array[0]
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) {
+      return b - a
+    })
+    return array[0]
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    array.forEach(element => {
+      sum += element
+    });
+    return (sum / array.length)
+  }
 }
